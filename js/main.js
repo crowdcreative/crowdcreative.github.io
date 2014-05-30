9c78cca47a3c9ce58686497b123b5b4b39385396
Jonatas Eduardo <jottaeblog@gmail.com>
2014-05-30 18:58:08 -0300

    Ajustes no index e remoção de peso morto

--- js/main.js
+++ js/main.js
@@ -1,38 +1,6 @@
 
 $(document).ready(function() {
-     $(window).load(function(){
-        $('.doc-loader').fadeOut('slow');
-     });
-	
-    $('.gallery').code4netslideshow({
-        'navigation' : [
-            {
-                'id' : 'gallery-nav',
-                'type' : 'bullet'           //bullet/arrow
-            }
-        ],
-        'slideInterval' : 5,
-        'pauseInterval' : 10, 
-        'type' : 'horizontal',
-        'autoSlide' : true
-    });
 
 	
-	$(function() {	
-		$('.roll').css('opacity','0');
-		$('.roll').hover(function () {
-				$(this).stop().animate({opacity: .7}, 'slow');
-			},
-			function () {
-				$(this).stop().animate({opacity: 0}, 'slow');
-			});
-	});
-	
-	$(function() {
-		if (window.PIE) {
-			$('.rounded').each(function() {
-				PIE.attach(this);
-			});
-		}
-	});
+
 });
